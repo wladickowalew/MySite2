af9d3faa06879452818e62fe5bab4775075cef19
Wladislaw <wladickowalew@gmail.com>
2022-03-13 17:49:37 +0300

    lesson 7.2 complete

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
     $('.bxslider').bxSlider({
         pager: false,
-    })
-})
+    });
+});
 
 $(document).ready(function(){
     $('.bxslider_team').bxSlider({
         controls: false,
-    })
-})
+    });
+});
+
+$(document).ready(function(){
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 200,
+        itemMargin: 25,
+        touch: true
+    });
+});
