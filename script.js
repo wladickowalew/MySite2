23081c4214e4ea9741f8d7acb616f75ed239fb94
Wladislaw <wladickowalew@gmail.com>
2022-04-03 18:29:51 +0300

    lesson 10 complete

--- script.js
+++ script.js
@@ -39,3 +39,36 @@ $(function(){
     };
     filterList.init();
 });
+
+
+$(document).ready(function($){
+    $('.primary-nav-trigger').on('click', function(){
+        $('.menu-icon').toggleClass('is-clicked');
+        $('.primary-nav').toggleClass('is-visible');
+        $('.body').toggleClass('overflow-hidden');
+    });
+});
+
+$('.primary-nav').on("click", "a", function(event){
+    event.preventDefault();
+    let id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body, html').animate({scrollTop: top}, 1000);
+});
+
+let headerHeight = $('.topheader').height();
+$(window).on('scroll', { previousTop: 0 },
+    function(){
+        let currentTop = $(window).scrollTop();
+        if(currentTop < this.previousTop){
+            if( !(currentTop > 0) && $('.topheader').hasClass('is-fixed')){
+                $('.topheader').removeClass('is-fixed');
+            }
+        }else{
+            if(currentTop > headerHeight && !$('.topheader').hasClass('is-fixed')){
+                $('.topheader').addClass('is-fixed');
+            }
+        }
+        this.previousTop = currentTop;  
+    }
+);
